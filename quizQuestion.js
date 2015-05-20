0c29d8749b82085a776548986edec33a2ffa2764
David Tuñón <daveyboy412@yahoo.com>
2015-05-20 15:26:10 -0500

    Array of questions for app.s

---
+++ quizQuestion.js
@@ -0,0 +1,10 @@
+var questions = [
+  ['How do you ask "Es eso la pluma tuya?"', 'Is that your pen?'],
+  ['How do you say "Eso es mi carterra."', "That is my purse."],
+  ['How do you ask "Que hora es?"', "What time is it?"],
+   ['How do you say "Son las tres."', "It is three o'clock."],
+   ['How do you ask "A que hora es la fiesta?"', "What time is the party?"],
+   ['How do you say "La renion es a las ocho."', "The meeting is at eight o'clock."],
+   ['How do you ask "Te gusta el cabello?"', "Do you like my hair?"],
+   ['How do you say "Me gusta el vestido."', "I like your dress."]
+];
